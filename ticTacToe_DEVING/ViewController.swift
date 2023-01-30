59b419a723ace50db5b914c014db2836c07de7c2
Dgriscow <dgrischow@students.mchenry.edu>
2023-01-30 14:23:07 -0600

    Initial Commit

---
+++ ticTacToe_DEVING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ticTacToe_DEVING
+//
+//  Created by Devin Grischow on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
